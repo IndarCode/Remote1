578ed5c1838606f94a6225b9ce6c4c6b0ebe4c3b
Indar <ikumar@empyra.com>
2023-10-16 16:38:38 +0530

    temp

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Main <code>src/App.js</code> and save to reload.
+          Main Code Push <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
